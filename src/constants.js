306fcbf29926b8452b223d067794c1bfe356895f
DaZhi <86814189+MR-DAzhi@users.noreply.github.com>
2025-12-27 18:15:33 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,6 @@
 export const APP_NAME = 'Sublink Worker';
 export const APP_VERSION = '2.2.0';
 export const GITHUB_REPO = 'https://github.com/MR-DAzhi/sublink-worker/';
-export const GITHUB_API_RELEASES = 'https://api.github.com/repos/7Sageer/sublink-worker/releases/latest';
 export const DOCS_URL = 'https://sublink.works';
 
 // SEO and metadata
